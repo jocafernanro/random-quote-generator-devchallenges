0e2ade2f3fdee9a0b57309c6d6cfecfc62dc014a
jocafernanro <jocafernanro@gmail.com>
2020-09-19 11:47:26 +0200

    Tailwind installed and project setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="font-bold text-blue-500">Testing tailwind</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
